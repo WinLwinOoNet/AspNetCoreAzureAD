bc4d5ebe3cacd855199fda6a6af5755893676a63
Win Lwin Oo <win.oo@via-christi.org>
2017-04-03 13:50:49 -0500

    Initial commit

---
+++ src/Presentation/Asp.Web/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/SignIn
+        public async Task SignIn()
+        {
+            if (HttpContext.User == null || !HttpContext.User.Identity.IsAuthenticated)
+            {
+                await HttpContext.Authentication.ChallengeAsync(
+                    OpenIdConnectDefaults.AuthenticationScheme,
+                    new AuthenticationProperties {RedirectUri = "/"});
+            }
+        }
+
+        // GET: /Account/SignOut
+        [HttpGet]
+        public async Task SignOut()
+        {
+            if (HttpContext.User != null && HttpContext.User.Identity.IsAuthenticated)
+            {
+                await HttpContext.Authentication.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+                await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme, 
+                    new AuthenticationProperties { RedirectUri = "/" });
+            }
+        }
+    }
+}
---
+++ src/Presentation/Asp.Web/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Asp.Web
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            // Add Authentication services.
+            services.AddAuthentication(sharedOptions => sharedOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            ConfigureAuth(app);
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public void ConfigureAuth(IApplicationBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme
+            });
+
+            string clientId = Configuration["Authentication:AzureAd:ClientId"],
+                aadInstance = Configuration["Authentication:AzureAd:AadInstance"],
+                tenant = Configuration["Authentication:AzureAd:Tenant"],
+                postLogoutRedirectUri = Configuration["Authentication:AzureAd:PostLogoutRedirectUri"],
+                authority = string.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                ClientId = clientId,
+                Authority = authority,
+                PostLogoutRedirectUri = postLogoutRedirectUri,
+                Events = new OpenIdConnectEvents
+                {
+                    OnRemoteFailure = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.Redirect("/Home/Error?message=" + context.Failure.Message);
+                        return Task.FromResult(0);
+                    },
+                },
+
+                // Store user's name in Name Claim instead of login email address
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                },
+            });
+        }
+    }
+}
